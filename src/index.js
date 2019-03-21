ba3331f211b9c4f2eadb48208521f91cb3326ebe
joseph.d.morris <joseph.d.morris@accenture.com>
2019-03-21 14:28:31 -0400

    bootstrap react app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+// Built from https://blog.usejournal.com/creating-a-react-app-from-scratch-f3c693b84658
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+)
